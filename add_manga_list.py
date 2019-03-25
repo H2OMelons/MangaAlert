7bb90b2db449a0d95d5a84b382bf608190c80aff
Ryan <rycintech@gmail.com>
2019-03-24 22:26:28 -0700

    Refactored add manga functionality into its own function

--- add_manga_list.py
+++ add_manga_list.py
@@ -50,35 +50,40 @@ def main():
       if user_input <= 0 or user_input > FINISH:
         errors.print_error(errors.INV_SEL)
       elif user_input == ADD:
-        manga_info = {}
-        # Go through all the categories necessary and prompt the user for the info
-        i = 0
-        while i < len(categories):
-          # Get the info for the category from the user
-          manga_info[categories[i]] = input(prompts[i])
-          # If the category requires a number input, check to make sure the user
-          # entered a number
-          if category_types[i] == int:
-            try:
-              manga_info[categories[i]] = int(manga_info[categories[i]])
-              # If the user enters something not between -1 and 6 for the upload
-              # day then print an error and decrement the counter
-              if i in upload_range and manga_info[categories[i]] not in range(-1, 7):
-                errors.print_error(errors.INV_SEL)
-                i -= 1
-            except:
-              # If the input was not a number, then print an error and decrement
-              # the counter
-              errors.print_error(errors.NOT_NUM)
-              i -= 1
-          # If the category requires a list, convert the comma separated
-          # string into a list
-          if category_types[i] == list:
-            manga_info[categories[i]] = manga_info[categories[i]].split(',')
-          i += 1
-        manga_list.append(manga_info)
+        add_manga(manga_list)
     except:
       errors.print_error(errors.NOT_NUM)
+
+# Function performs asks user for info about the manga they want to add
+# to the list
+def add_manga(manga_list):
+  manga_info = {}
+  # Go through all the categories necessary and prompt the user for the info
+  i = 0
+  while i < len(categories):
+    # Get the info for the category from the user
+    manga_info[categories[i]] = input(prompts[i])
+    # If the category requires a number input, check to make sure the user
+    # entered a number
+    if category_types[i] == int:
+      try:
+        manga_info[categories[i]] = int(manga_info[categories[i]])
+        # If the user enters something not between -1 and 6 for the upload
+        # day then print an error and decrement the counter
+        if i in upload_range and manga_info[categories[i]] not in range(-1, 7):
+          errors.print_error(errors.INV_SEL)
+          i -= 1
+      except:
+        # If the input was not a number, then print an error and decrement
+        # the counter
+        errors.print_error(errors.NOT_NUM)
+        i -= 1
+    # If the category requires a list, convert the comma separated
+    # string into a list
+    if category_types[i] == list:
+      manga_info[categories[i]] = manga_info[categories[i]].split(',')
+    i += 1
+  manga_list.append(manga_info)
 
 # Prints a menu on the terminal with the given menu items, with each
 # item given an index in the order as in menu_items
