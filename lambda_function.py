45bde96a5772cc1e79125f977ae36f1c6f9d0d68
Ryan <rycintech@gmail.com>
2019-04-04 19:07:44 -0700

    Now if a user inputted multiple entries for the same manga (with different poster) when any one of the poster uploads a new chapter, all entries have their most recent chapter value updated. Reddit instance now uses refresh token instead of account username/password

--- lambda_function.py
+++ lambda_function.py
@@ -31,35 +31,53 @@ def lambda_handler(event, context):
     )
     mangas.extend(response['Items'])
 
+  # Build a dictionary where the key is a manga name and the value is
+  # a list of indicies corresponding to the manga's index in the mangas array
+  # Ex:
+  #   mangas = [a, b, c, a]
+  #   manga_name_tracker = {'a':[0,3], 'b':[1], 'c':[2]}
+  manga_name_tracker = {}
+  for i in range(len(mangas)):
+    manga_name = mangas[i]['manga_name']['S']
+    if manga_name_tracker.get(manga_name):
+      manga_name_tracker[manga_name].append(i)
+    else:
+      manga_name_tracker[manga_name] = [i]
+
   client_id = os.environ.get('REDDIT_CLIENT_ID')
   client_secret = os.environ.get('REDDIT_CLIENT_SECRET')
-  password = os.environ.get('REDDIT_PASSWORD')
   user_agent = os.environ.get('REDDIT_USER_AGENT')
-  username = os.environ.get('REDDIT_USERNAME')
+  refresh_token = os.environ.get('REDDIT_REFRESH_TOKEN')
 
   # Build an instance of praw (Reddit api wrapper)
   reddit = praw.Reddit(
     client_id = client_id,
     client_secret = client_secret,
-    password = password,
     user_agent = user_agent,
-    username = username
+    refresh_token = refresh_token
   )
 
-  # List that will contain all mangas that had their chapter updated
-  updated = []
+  # Dictionary where the key is the name of the manga that was updated and the
+  # value is the new most recent chapter number
+  updated = {}
   for manga in mangas:
+    # Get the name of the manga
+    og_manga_name = manga['manga_name']['S']
+
+    # If already detected that the manga has been updated, skip to next manga
+    if updated.get(og_manga_name):
+      continue
+
     # Get the reddit user who posts the weekly chapter
     redditor = reddit.redditor(manga['poster']['S'])
     for submission in redditor.submissions.new(limit=10):
       title = submission.title.lower()
-      og_manga_name = manga['manga_name']['S']
       manga_name = og_manga_name.lower()
       current_chapter = int(manga['most_recent_chapter']['N']) + 1
       # If the user created a reddit post with the manga name and chapter in the title
       # It is probably the post for the weekly chapter
       if manga_name in title and str(current_chapter) in title and 'prediction' not in title:
-        updated.append(manga)
+        updated[og_manga_name] = current_chapter
         # Send text that new chapter has been posted
         if os.environ.get('ENV') == 'PROD':
           sns = boto3.client('sns')
@@ -70,23 +88,25 @@ def lambda_handler(event, context):
         else:
           print(submission.shortlink)
 
-  # For each manga that had their chapter updated, update the
-  # most_recent_chapter category in dynamodb
-  for manga in updated:
-    response = dynamodb.update_item(
-      TableName = 'manga_list',
-      Key = {
-        'manga_name' : manga['manga_name'],
-        'poster' : manga['poster']
-      },
-      ExpressionAttributeNames = {
-        '#M' : 'most_recent_chapter'
-      },
-      ExpressionAttributeValues = {
-        ':m' : {'N' : str(int(manga['most_recent_chapter']['N']) + 1)}
-      },
-      UpdateExpression = 'SET #M = :m'
-    )
+  # If a manga was updated, update all entries with the same name to have
+  # the new most recent chapter
+  for manga_name in updated:
+    for manga_ind in manga_name_tracker[manga_name]:
+      manga = mangas[manga_ind]
+      response = dynamodb.update_item(
+        TableName = 'manga_list',
+        Key = {
+          'manga_name' : manga['manga_name'],
+          'poster' : manga['poster']
+        },
+        ExpressionAttributeNames = {
+          '#M' : 'most_recent_chapter'
+        },
+        ExpressionAttributeValues = {
+          ':m' : {'N' : str(int(manga['most_recent_chapter']['N']) + 1)}
+        },
+        UpdateExpression = 'SET #M = :m'
+      )
 
 if os.environ.get('ENV') != 'PROD':
   lambda_handler({},{})
