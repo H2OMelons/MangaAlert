d3da727757b40fb1bb40703dc89faae7ba373220
Ryan <rycintech@gmail.com>
2019-06-25 15:59:33 -0700

    Modified function to use praw again and only pull from /r/manga

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import boto3
 import os
-import requests
-import time
+import datetime
+import praw
 
 dynamodb = None
 
@@ -11,8 +11,34 @@ else:
   dynamodb = boto3.client('dynamodb', endpoint_url = 'http://localhost:8000')
 
 def lambda_handler(event, context):
-  # number of milliseconds in a day
   table_name = 'manga_list'
+  lambda_client = None
+  arn = ''
+  # Get UTC time and subtract 6 minutes
+  utc_time = (datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()
+  utc_time -= 60 * 6
+
+  if os.environ.get('ENV') == 'PROD':
+    lambda_client = boto3.client('lambda')
+    arn = os.environ.get('MANGA_ALERT_LAMBDA_ARN')
+    tags = lambda_client.list_tags(
+      Resource = arn
+    )
+    tags = tags['Tags']
+    utc_time = int(tags['last_retrieved_time'])
+
+  # Create an instance of the reddit client using praw
+  client_id = os.environ.get('REDDIT_CLIENT_ID')
+  client_secret = os.environ.get('REDDIT_CLIENT_SECRET')
+  refresh_token = os.environ.get('REDDIT_REFRESH_TOKEN')
+  user_agent = os.environ.get('REDDIT_USER_AGENT')
+
+  reddit = praw.Reddit(
+    client_id = client_id,
+    client_secret = client_secret,
+    refresh_token = refresh_token,
+    user_agent = user_agent
+  )
 
   print("Retrieving items from DynamoDB")
   response = dynamodb.scan(TableName = table_name)
@@ -25,27 +51,14 @@ def lambda_handler(event, context):
     )
     mangas.extend(response['Items'])
 
-  # Create list of subreddits
-  # Each subreddit entry has a list. Each entry in the list is a dictionary that contains
-  # The manga name, most recent chapter, and additional filters to apply
-  subreddits_list = {}
+  # Create a dictionary of the mangas where the key is the name and
+  # the value is the most recent chapter
+  manga_dict = {}
+
   for manga in mangas:
-    for subreddit in manga['subreddits']['L']:
-      subreddit = subreddit['S']
-      additional_filters = []
-      if manga.get('additional_filters'):
-        additional_filters = {x['M']['subreddit']['S'] : x['M'] for x in manga['additional_filters']['L']}
-
-      manga_item = {
-        'name' : manga['manga_name']['S'].lower(),
-        'most_recent_chapter' : manga['most_recent_chapter']['N'],
-        'additional_filters' : additional_filters
-      }
-
-      if subreddits_list.get(subreddit):
-        subreddits_list[subreddit].append(manga_item)
-      else:
-        subreddits_list[subreddit] = [manga_item]
+    name = manga['manga_name']['S']
+    most_recent_chapter = manga['most_recent_chapter']['N']
+    manga_dict[name] = most_recent_chapter
 
   # Dictionary where the key is the name of the manga that was updated and the
   # value is the new most recent chapter number
@@ -54,106 +67,31 @@ def lambda_handler(event, context):
   # Array to keep track of links to the updated chapter
   links = []
 
-  # Loop through all subreddits and get the newest 20 posts
-  for subreddit in subreddits_list:
-    # Get all the mangas that we want to check if a new chapter has been
-    # posted to the subreddit
-    mangas_list = subreddits_list[subreddit]
-    # Remove all mangas that we already found updates for (in other subreddits)
-    i = 0
-    while i < len(mangas_list):
-      if updated.get(mangas_list[i]['name']):
-        mangas_list.pop(i)
-        i -= 1
-      i += 1
+  update_lambda_tag = True
+  last_retrieved_time = ''
+  disc_submissions = []
+  print('Getting most recent posts from /r/manga')
+  # Loop through 15 most recent posts in manga subreddit
+  for submission in reddit.subreddit('manga').new(limit=15):
+    if update_lambda_tag:
+      update_lambda_tag = False
+      last_retrieved_time = str(submission.created_utc + 1)
+    tag = submission.link_flair_text
+    if type(tag) == str and tag.lower() == 'disc' and submission.created_utc >= utc_time:
+      disc_submissions.append(vars(submission))
 
-    # If we already found updates for all mangas that we want to check for in this
-    # subreddit, skip the subreddit
-    if len(mangas_list) == 0:
+  # Go through all the submissions from oldest to newest
+  for i in range(len(disc_submissions) - 1, -1, -1):
+    submission = disc_submissions[i]
+    # Skip the submission if it was removed
+    if 'selftext' not in submission or submission['selftext'] == '[deleted]' or not submission['is_crosspostable'] or submission['selftext'] == '[removed]':
       continue
 
-    print('Getting most recent posts from /r/' + subreddit)
-    # Get the most recent 20 posts
-    submissions = []
-    # Get the time so we can time how long it takes to get the submissions
-    start = time.time()
-    submission = requests.get(
-      'https://api.pushshift.io/reddit/search/submission',
-      params = {
-        'subreddit' : subreddit,
-        'size' : 20,
-        'sort' : 'desc',
-        'sort_type' : 'created_utc'
-      }
-    )
-    end = time.time()
-    print('Finished in ' + str(round(end - start, 4)) + ' seconds')
-    submission = submission.json()['data']
-    submissions.extend(submission)
+    title = submission['title'].lower()
 
-    # Loop through all posts to the subreddit and check the titles to see if
-    # it is the new chapter for the mangas we are looking for
-    for submission in submissions:
-
-      # Skip the submission if it was removed
-      if 'selftext' not in submission or submission['selftext'] == '[deleted]' or not submission['is_crosspostable'] or submission['selftext'] == '[removed]':
-        continue
-
-      title = submission['title'].lower()
-
-      for manga in mangas_list:
-        manga_name = manga['name']
-        if updated.get(manga_name):
-          continue
-        current_chapter = int(manga['most_recent_chapter']) + 1
-        additional_filters = manga['additional_filters']
-        additional_filters = additional_filters[subreddit] if additional_filters.get(subreddit) else {}
-        # or filters are keywords where we want at least one to appear in the title
-        or_filters = [manga_name]
-        if additional_filters.get('or'):
-          or_filters.extend([x['S'] for x in additional_filters['or']['L']])
-        # and filters are keywords, where we want ALL the keywords in the list to appear in the title
-        and_filters = []
-        if additional_filters.get('and'):
-          and_filters.extend([x['S'] for x in additional_filters['and']['L']])
-        if '#chapter_number' in and_filters:
-          and_filters[and_filters.index('#chapter_number')] = str(current_chapter)
-        # Exclude filters are keywords that tell us that the post is not the one we want
-        exclude_filters = []
-        if additional_filters.get('exclude'):
-          exclude_filters = [x['S'] for x in additional_filters['exclude']['L']]
-
-        # If any of the exclude filters are in the title, then this post is not the one we wnat
-        # so continue
-        exclude_found = False
-        for exclude in exclude_filters:
-          if exclude in title:
-            exclude_found = True
-            break
-
-        # If any of the and filters are not in the title, continue
-        and_found = False
-        for a_filter in and_filters:
-          if a_filter not in title:
-            and_found = True
-            break
-
-        # If none of the or filters is in title, title isn't the one we want
-        or_found = True
-        for o_filter in or_filters:
-          if o_filter in title:
-            or_found = False
-            break
-
-        # If the title met all conditions, then it is the one we want
-        if not exclude_found and not and_found and not or_found:
-          updated[manga_name] = current_chapter
-          links.append({
-            'url' : submission['full_link'],
-            'msg'  : manga_name + ' Chapter ' + str(current_chapter)
-          })
-
-  print('Finished going through all subreddits')
+    for manga, chapter in manga_dict.items():
+      if manga not in updated and manga in title and str(chapter + 1) in title:
+        updated[manga] = chapter + 1
 
   if len(links) > 0:
     print('Sending update SMS')
@@ -171,6 +109,15 @@ def lambda_handler(event, context):
       for link in links:
         print(link)
 
+  # Update the tag with the newest time
+  if os.environ.get('ENV') == 'PROD':
+    print('Updating tag')
+    lambda_client.tag_resource(
+      Resource = arn,
+      Tags = {
+        'last_retrieved_time' : last_retrieved_time
+      }
+    )
 
   # If a manga was updated, update all entries with the same name to have
   # the new most recent chapter
